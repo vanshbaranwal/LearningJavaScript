3686e308e3a0e96a1ec9a9e2ceb1170c96e45d63
vanshbaranwal <vanshveerbaranwal@gmail.com>
2025-07-23 23:36:44 +0530

    problem on loops

--- loops/problem6.js
+++ loops/problem6.js
@@ -1,6 +1,6 @@
-// find the sum of the given digits of a number.
+//program to reverse a given number by the user.
 
-let pr = prompt("write a numnber to get the total of the digits of the given number : ");
+let pr = prompt("write a number to get the reverse of the digits of the given number : ");
 
 if(pr === null){
     console.log("cancelled by the user.");
@@ -10,15 +10,16 @@ if(pr === null){
         console.log("invalid input");
     }else{
         if(number>0){
-            let sum=0;
+            let rev = 0;
             while(number>0){
                 let rem = number%10;
-                sum+=rem;
+                rev = rev*10 + rem;
                 number = Math.floor(number/10);
             }
-            console.log(sum);
+            console.log(rev);
         }else{
             console.log("invalid input.");
         }
     }
 }
+
