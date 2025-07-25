f48170449bfe5f5585c6dc9ba941dfb5bf80d669
vanshbaranwal <vanshveerbaranwal@gmail.com>
2025-07-25 23:53:48 +0530

    started with patterns

---
+++ patterns/problem1.js
@@ -0,0 +1,18 @@
+// 
+// let prompt = require("prompt-sync")();
+// // import prompt from 'prompt-sync'();
+
+// let n = Number(prompt("enter a number : "));
+
+
+for(let i=1; i<=5; i++){
+    process.stdout.write("*");
+}
+console.log();
+for(let i=1; i<=5; i++){
+    process.stdout.write("*");
+}
+console.log()
+for(let i=1; i<=5; i++){
+    process.stdout.write("*");
+}
